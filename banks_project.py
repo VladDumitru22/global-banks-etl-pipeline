d85cbaf9148fdcf956f9e751d82020fc956b5437
Dumitru Vlad-Mihai <vlad.liviu22@gmail.com>
2026-03-25 21:58:48 +0200

    Add extract function

--- banks_project.py
+++ banks_project.py
@@ -1,6 +1,9 @@
 import os
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
 from datetime import datetime
-from config import LOG_PATH
+from config import LOG_PATH, URL, TABLE_ATTRIBS_BEFORE
 
 def log_progress(message):
     ''' This function logs the mentioned message of a given stage of the
@@ -9,5 +12,40 @@ def log_progress(message):
     os.makedirs(os.path.dirname(LOG_PATH), exist_ok=True)
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     with open(LOG_PATH, 'a') as f:
-        f.write(f"{timestamp} : {message}")
+        f.write(f"{timestamp} : {message}\n")
+
+def extract(url, table_attribs):
+    ''' This function aims to extract the required
+    information from the website and save it to a data frame. The
+    function returns the data frame for further processing. '''
+    
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception("Failed to fetch data")
+    data = BeautifulSoup(response.text, 'html.parser')
+
+    tables = data.find_all('tbody')
+    rows = tables[0].find_all('tr')
+
+    data_list = []
+    for row in rows:
+        cols = row.find_all('td')
+        if len(cols) != 0:
+            data_list.append({
+                'Name': cols[1].text.strip(),
+                'MC_USD_Billion': cols[2].text.strip(),
+            })
+    
+    df = pd.DataFrame(data_list, columns=table_attribs)
+
+    return df
+
+def transform(df, csv_path):
+    ''' This function accesses the CSV file for exchange rate
+	information, and adds three columns to the data frame, each
+	containing the transformed version of Market Cap column to
+	respective currencies'''
+
+    return df
+
 
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-URL = "https://web.archive.org/web/20230908091635 /https://en.wikipedia.org/wiki/List_of_largest_banks"
+URL = "https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks"
 TABLE_ATTRIBS_BEFORE = ['Name', 'MC_USD_Billion']
 TABLE_ATTRIBS_FINAL = ['Name', 'MC_USD_Billion', 'MC_GDB_Billion', 'MC_EUR_Billion', 'MC_INR_Billion']
 TABLE_NAME = 'Largest_banks'
-LOG_PATH = 'code_log.txt'
+LOG_PATH = './code_log.txt'
 DB_NAME = 'Banks.db'
 CSV_PATH = './Largest_banks_data.csv'
